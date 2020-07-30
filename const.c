98ed0324ae53512ae91945c319d72826b670c76d
nonaliasMAC <nonaliasmac@nonaliasMACui-Mac.local>
2020-07-31 01:48:22 +0900

    const

---
+++ const.c
@@ -0,0 +1,16 @@
+#ifndef __CONST_H
+# define __CONST_H
+
+# define KEY_PRESSED 2
+# define KEY_RELEASED 3
+# define WIN_WIDTH 500
+# define WIN_HEIGHT 500
+# define LEFT_ARROW 123
+# define RIGHT_ARROW 124
+# define KEY_A 0
+# define KEY_W 13
+# define KEY_D 2
+# define KEY_S 1
+# define KEY_R 15
+
+#endif
