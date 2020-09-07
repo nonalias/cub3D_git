fed7e7ae9cabb095d6de3207302c6dfa07907881
nonaliasMAC <nonaliasmac@nonaliasMACui-Mac.local>
2020-09-07 15:36:02 +0900

    map validation proto

--- engine/validation.c
+++ engine/validation.c
@@ -1,9 +1,5 @@
 #include "../cub3d.h"
 
-int		is_map_valid(t_game *game)
-{
-}
-
 int		is_option_valid(t_game *game)
 {
 	int		i;
@@ -20,10 +16,31 @@ int		is_option_valid(t_game *game)
 	return (1);
 }
 
+int		is_map_valid(t_game *game)
+{
+	int		i;
+
+	i = 0;
+	while (i < game->map.rows)
+	{
+		if (!my_map[i][0] || !my_map[i][game->map.columns - 1])
+			return (0);
+		i++;
+	}
+	i = 0;
+	while (i < game->map.columns)
+	{
+		if (!my_map[0][i] || !my_map[game->map.rows - 1][i])
+			return (0);
+		i++;
+	}	
+	return (1);
+}
+
 void	validation(t_game *game)
 {
 	if (!is_option_valid(game))
 		error_exit(game, OPTION_ERROR, "option error");
-	//if (!is_map_valid(game))
-		//error_exit(game, MAP_ERROR);
+	if (!is_map_valid(game))
+		error_exit(game, MAP_ERROR, "map error");
 }
